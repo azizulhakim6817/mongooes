e17fa99ebbb17404d4832aae90f0ceefe1e38a06
azizulhakim6817 <azizulhakim68178@gmail.com>
2026-06-24 03:28:51 -0600

    add

--- src/server.ts
+++ src/server.ts
@@ -7,13 +7,12 @@ async function server() {
     await mongoose.connect(config.database_url as string);
 
     console.log("Database is connected");
-
-    app.listen(config.port, () => {
-      console.log(`Server is running http://localhost:${config.port}`);
-    });
   } catch (error) {
     console.error("Server error:", error);
   }
 }
-
 server();
+
+app.listen(config.port, () => {
+  console.log(`Server is running http://localhost:${config.port}`);
+});
