92175648c8681e1807ec4bdb115992a125f650ec
azizulhakim6817 <azizulhakim68178@gmail.com>
2026-06-24 03:20:04 -0600

    add

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -6,6 +6,6 @@ dotenv.config();
 export default {
   node_env: process.env.NODE_ENV,
   port: process.env.PORT,
-  database_url: process.env.DATABASE_URL as string,
+  database_url: process.env.DATABASE_URL,
   base_url: process.env.URL,
 };
--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,11 @@ import mongoose from "mongoose";
 import app from "./app";
 import config from "./app/config/index";
 
-
 async function server() {
   try {
+    if (!config.database_url) {
+      throw new Error("DATABASE_URL is not defined");
+    }
     await mongoose.connect(config.database_url);
 
     console.log("Database is connected");
